cbd29ff35a35649245a18bebffb0e1fd7a71590e
zorator <vivien.auguy@epf-projets.fr>
2014-09-11 15:25:32 +0200

    Library added, Computer DAO finalized

--- src/fr/epf/computerdatabase/dao/ComputerDAO.java
+++ src/fr/epf/computerdatabase/dao/ComputerDAO.java
@@ -1,5 +1,54 @@
 package fr.epf.computerdatabase.dao;
 
-public class ComputerDAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import fr.epf.computerdatabase.domain.Computer;
+
+public enum ComputerDAO {
+
+	INSTANCE;
+
+	private EntityManagerFactory emf;
+
+	private ComputerDAO() {
+		emf = Persistence.createEntityManagerFactory("computerdatabase-PU");
+	}
+
+	private EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+
+	public static ComputerDAO getInstance() {
+		return INSTANCE;
+	}
+
+	public void create(Computer computer) {
+		EntityManager em = null;
+		try {
+			em = getEntityManager();
+			em.getTransaction().begin();
+			em.persist(computer);
+			em.getTransaction().commit();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+
+	public List<Computer> getAll() {
+		EntityManager em = null;
+		try {
+			em = getEntityManager();
+			return em.createQuery("SELECT c FROM Computer c").getResultList();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
 }
