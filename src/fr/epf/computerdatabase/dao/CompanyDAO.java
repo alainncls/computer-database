1eda82437ad1f77d03b918aae4ec33613a68abf8
alainncls <alain.nicolas.perso@gmail.com>
2014-09-11 15:54:30 +0200

    Well Done

---
+++ src/fr/epf/computerdatabase/dao/CompanyDAO.java
@@ -0,0 +1,54 @@
+package fr.epf.computerdatabase.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import fr.epf.computerdatabase.domain.Company;
+
+public enum CompanyDAO {
+
+	INSTANCE;
+
+	private EntityManagerFactory emf;
+
+	private CompanyDAO() {
+		emf = Persistence.createEntityManagerFactory("computerdatabase-PU");
+	}
+
+	private EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+
+	public static CompanyDAO getInstance() {
+		return INSTANCE;
+	}
+
+	public void create(Company company) {
+		EntityManager em = null;
+		try {
+			em = getEntityManager();
+			em.getTransaction().begin();
+			em.persist(company);
+			em.getTransaction().commit();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+
+	public List<Company> getAll() {
+		EntityManager em = null;
+		try {
+			em = getEntityManager();
+			return em.createQuery("SELECT c FROM Company c").getResultList();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
+}
--- src/fr/epf/computerdatabase/service/CompanyDBService.java
+++ src/fr/epf/computerdatabase/service/CompanyDBService.java
@@ -1,5 +1,34 @@
 package fr.epf.computerdatabase.service;
 
-public class CompanyDBService {
+import java.util.List;
 
+import fr.epf.computerdatabase.dao.CompanyDAO;
+import fr.epf.computerdatabase.domain.Company;
+
+public class CompanyDBService {
+	private static CompanyDBService instance = null;
+
+	// Static : has to be call without an instance
+	public static CompanyDBService getInstance() {
+
+		if (instance == null) {
+			// If there is no instance yet just, create it.
+			instance = new CompanyDBService();
+		}
+		return instance;
+	}
+
+	private CompanyDBService() {
+
+	}
+
+	public void create(Company company) {
+		companyDAO.create(company);
+	}
+
+	private CompanyDAO companyDAO = companyDAO.getInstance();
+
+	public List<Company> getAll() {
+		return companyDAO.getAll();
+	}
 }
